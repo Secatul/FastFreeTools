710705994912410b2653cb0e0dd814a7a17c3101
lcscostadev <thelucascostadasilva@gmail.com>
2024-10-24 19:40:28 -0300

    google analytics

--- app/layout.js
+++ app/layout.js
@@ -27,17 +27,15 @@ function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9N9VQ84833"></script>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag() {
-          dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-        gtag('config', 'G-9N9VQ84833');
-      `,
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+            `,
           }}
         />
       </Head>
@@ -49,7 +47,7 @@ function RootLayout({ children }) {
             {children}
             <Toaster />
             <Analytics />
-            <SpeedInsights/>
+            <SpeedInsights />
             <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID} />
           </ThemeProvider>
         </Provider>
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,26 @@
 import { ThemeProvider } from 'next-themes'
 import { Toaster } from "@/components/ui/toaster"
 import '../styles/globals.css';
+import Head from 'next/head';
+
 
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
+      <Head>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+            `,
+          }}
+        />
+      </Head>
       <Component {...pageProps} />
       <Toaster />
     </ThemeProvider>
